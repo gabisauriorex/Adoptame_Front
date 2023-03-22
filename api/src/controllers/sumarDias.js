5caf17e2b61fb17a051821f00973a4202ab3cf31
arielZarate <arieltecnico@gmail.com>
2023-03-22 10:52:18 -0300

    segundo commit

--- api/src/controllers/sumarDias.js
+++
@@ -1,8 +0,0 @@
-/* Función que suma o resta días a una fecha, si el parámetro
-   días es negativo restará los días*/
-   const sumarDias = (fecha, dias) => {
-    fecha.setDate(fecha.getDate() + dias);
-    return fecha;
-  }
-
-  module.exports = sumarDias;
