95a09b672d2b29980bc1f91f4955cbe3a16c74ba
Mateo Ossorio Arana <matossoara@gmail.com>
2023-03-23 11:53:28 -0300

    modificaciones a estilos
    estilos modificados
    Filtros alineados
    detalle creado
    cards modificados los valores que se muestran y el estilo

--- client/src/components/Card/CardPet.jsx
+++ client/src/components/Card/CardPet.jsx
@@ -1,8 +1,4 @@
-
-
-
 import { styled } from '@mui/material/styles';
-
 import {Paper,Grid ,Box}from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -10,9 +6,7 @@ import Pets from '@mui/icons-material/Pets'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import {Link} from 'react-router-dom'
-
 import './CardPet.css'
-
 
 const Img = styled('img')({
   margin:'1px',
@@ -22,52 +16,45 @@ const Img = styled('img')({
   padding:'1px'
 });
 
-
-
-
-function CardPet({id,image,name,timewait,description }) 
+function CardPet({id,image,name,sexo,edad,description }) 
 {
-  const path=`/pets/:${id}`;
-     return (
- 
-            <Paper className='containerCardPet'
-                >
-
-                  <Grid >
-                  <Typography  variant="h6" component="div"  color="#0f4d71" /* sx={{backgroundColor:"#ffa733" ,borderRadius:5 ,margin:"3px"}} */>
-                      {name}       <FontAwesomeIcon icon={faPaw} bounce/> 
-                  </Typography>
-                 
-                 
+  const path=`/pets/${id}`; 
+     return ( 
+              <Paper className='containerCardPet'>
+                  <Grid>
+                      <Typography  variant="h6" component="div"  color="#0f4d71" /* sx={{backgroundColor:"#ffa733" ,borderRadius:5 ,margin:"3px"}} */>
+                          {name}       <FontAwesomeIcon icon={faPaw} bounce/> 
+                      </Typography>                                 
                   </Grid>
                   <Grid item xs={12} sm={4} md={4}>
-                  <Button component={Link} to={path} >
-                  <Img alt="complex" src={image} />
-                  </Button>
-                </Grid>
-                <Grid item xs={12} sm={3} md={4}  className='containerText' >
-                  <Box sx={{ marginLeft:5, display:'flex',flexDirection:'row' ,justifyContent:'center',alignItems:'center'}}>
-                  <Typography variant="subtitle2" gutterBottom>
-                    {timewait}
-                  </Typography>
-                  </Box>
-                </Grid>
-              </Paper>
-  
+                      <Button component={Link} to={path} >
+                      <Img alt="complex" src={image} />
+                      </Button>
+                  </Grid>
+                  <Grid item xs={12} className='containerText' >
+                      <Box sx={{width:'100%', display:'flex',flexDirection:'row' ,justifyContent:'space-around',alignItems:'center'}}>
+                      <Typography
+                          noWrap
+                          variant="subtitle2" 
+                          padding={1}                          
+                      >
+                          {edad}
+                      </Typography>
+                      <Typography
+                          variant="subtitle2"
+                          padding={1}
+                           >
+                          {sexo}
+                      </Typography>
+                      </Box>
+                  </Grid>
+              </Paper>  
   )
 }
-
 export default CardPet
 
 
-
-
-
 /* 
-
-
-
-
   <Paper
             key={id}
            sx={{
@@ -78,8 +65,7 @@ export default CardPet
             flexGrow: 1,
             borderRadius:1
            backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-         
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',         
             }}
             >
              <Grid container spacing={5}> 
@@ -100,25 +86,9 @@ export default CardPet
                       <Typography variant="body2" color="text.secondary">
                         {description}
                       </Typography>
-                   </Grid> 
-                   
-                    </Grid>
-             
+                   </Grid>                    
+                    </Grid>             
                   </Grid>
                 </Grid> 
                </Paper>
-
-
-
-
-
-
 */
-
-
-
-
-
-
-
-
--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,146 +1,94 @@
- import React ,{ useEffect }from "react";
+import React ,{ useEffect }from "react";
 import { Link, useParams } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-
 import {getDetails}from '../../Redux/Actions/actions_pets' 
 import  '../Detail/Detail.css' 
-
-
 import { styled } from '@mui/material/styles';
 import {Card,Grid,Box} from '@mui/material';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+const Img = styled('img')({
+  margin:'1px',
+  width:'200px',
+  height:'140px',
+  borderRadius:"5px",
+  padding:'1px'
+});
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
+const { expand, ...other } = props;
+return <IconButton {...other} />;
+        })
+  (({ theme, expand }) => ({
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
   marginLeft: 'auto',
   transition: theme.transitions.create('transform', {
     duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-
+    }),
+    }));
+    
 export default function Detail(){
-   const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const {id} = useParams();
     useEffect(() => {
         dispatch(getDetails(id));   
     },[dispatch]);
    const detail = useSelector((state) => state.detail);
-
+   const ejemplo = {
+		  id: "1",
+      name: "Pichi",
+      image:
+        "https://www.lavanguardia.com/files/og_thumbnail/uploads/2022/03/15/62308d3f4a45d.png",
+      description: "una perra buena ",
+      timewait: "20/03/2015",
+      breed: "cokkie",
+      animal: "perro",
+      height: 7,
+      color: "Otro",
+      identified: true,
+      vacunas: "todas",
+      edad: "4 años",
+      sexo: "Hembra",
+      especie: "perro",
+	}
     return (
         <div >  
- <Box className="containerDetail">
-
-
-         
-    <Grid container /* rowSpacing={1} columnSpacing={{ xs: 0, sm: 0, md: 0 }} */ >
- 
-        
-        <Grid container  item xs={5} md={4} >
-         <Card>
-         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-         
-        </CardContent>
-         </Card>
-        </Grid>
-          <Grid item xs={4} sm={9} md={4}>
-
-        
-<Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-    {/*   <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent> */}
-          </Card>
-        </Grid> 
-         <Grid item xs={4} sm={9} md={4}>
-          ultima caja
-        </Grid>
-    
-      </Grid> 
-
-
-      
-      <div className="contenedorBoton">
-                 <Link  to={'/home'} >
-               <button className="botonVolver">VOLVER</button>
-               </Link>
-            </div>  
-
- </Box>
-
-
-
-
-
-
-
-
-
-
-                
-         
+            <Box  className="containerDetail">
+                <Box sx={{  display:"flex" }}>
+                    <Grid container  /* rowSpacing={1} columnSpacing={{ xs: 0, sm: 0, md: 0 }} */ >
+                        <Grid container  item xs={5} md={100} >
+                        <Card>
+                            <CardContent>
+                                <Img alt="complex" src={ejemplo.image}/> {/* cambiar ek ejemplo.etc por detail.etc cuando este la base del back */}
+                                <Typography paragraph>Nombre: {ejemplo.name}</Typography>
+                                <Typography paragraph>Descripcion: {ejemplo.description}</Typography>
+                                <Typography paragraph>Raza: {ejemplo.breed}</Typography>
+                                <Typography paragraph>Altura: {ejemplo.height}</Typography>
+                                <Typography paragraph>Color: {ejemplo.color}</Typography>
+                                <Typography paragraph>Vacunas: {ejemplo.vacunas}</Typography>
+                                <Typography paragraph>Edad: {ejemplo.edad}</Typography>
+                                <Typography paragraph>Sexo: {ejemplo.sexo}</Typography>
+                                <Typography paragraph>Animal: {ejemplo.especie}</Typography>                               
+                            </CardContent>
+                        </Card>
+                        </Grid>                                   
+                    </Grid>       
+                    <div className="contenedorBoton">
+                      <Link  to={'/home'}>
+                          <Stack direction="row">
+                            <Button variant="contained">Volver</Button>
+                          </Stack>                           
+                      </Link>
+                      <Link  to={'/CreatePet'}>
+                          <Stack direction="row">
+                            <Button variant="contained">Adoptame</Button>
+                          </Stack>                           
+                      </Link>
+                    </div>  
+                </Box> 
+            </Box>         
         </div> 
-             )}
-            
-        
-
-
-
-             
-          {/*  <div className="detail_container">
-                 <h4>{detail.name}</h4> 
-                 <img className='imagen p' src ={detail.image} height="250px" width='250px' alt='not found'/>
-                 <p className="title" >{detail.description}</p>
-                 <p className="p"> <span className="text color">Color </span> <span className="text ">{detail.color}</span></p>
-                 <p className="p"> <span className="text color">Altura </span> <span className="text ">{detail.height}</span></p>
-                 <p className="p"> <span className="text color">Peso </span> <span className="text ">{detail.weight}</span></p>
-                 <p className="p"> <span className="text color">Encontrado el dia: </span> <span className="text ">{detail.timewait}</span></p>
-                 <p className="p"> <span className="text color">Especie </span> <span className="text ">{detail.breed}</span></p>
-                 <p className="p"> <span className="text color">Adoptado </span> <span className="text ">{detail.adopted}</span></p>
-                 <p className="p"> <span className="text color">Identificado </span> <span className="text ">{detail.identified}</span></p>
-                 <p className="p"> <span className="text color">Edad </span> <span className="text ">{detail.age}</span></p>
-    
-            </div> 
- */}
+      )}
